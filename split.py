0277af0db56aee6d32c086958cca172d77120a81
蔡瑋倫 <william041107@gmail.com>
2020-12-15 23:05:22 +0800

    annotation filter and os rename update

--- split.py
+++ split.py
@@ -18,6 +18,7 @@ labels_folder_path = original_folder_path + "/labels"
 
 empty_json = {"images": [], "type": "instances", "annotations": [], "categories": [] }
 annotations_list = [359, 370, 381, 382, 371, 379, 216, 218, 221, 224, 226, 229, 232, 235, 238, 241, 244, 247, 250, 287, 311, 335, 337, 341, 344, 347, 350, 354, 356, 259, 362, 365, 368, 372, 373, 414, 417, 383, 387, 384, 389, 386, 385, 388, 394, 390, 391, 392, 393, 432, 447, 448, 449, 450, 452, 451, 456, 455, 453, 458, 454, 457, 472, 479, 533, 540, 544, 697, 732, 865, 867, 777, 778, 780, 779, 781, 782, 784, 783, 785, 786, 787, 788, 789, 790, 791, 793, 796, 792, 794, 795, 797, 798, 799, 800, 604, 606, 607, 605, 609, 608, 611, 610, 621, 615, 612, 622, 614, 613, 616, 617, 618, 619, 620, 623, 624, 625, 626, 627, 1, 2, 3, 4, 9, 10, 7, 6, 8, 5, 13, 12, 11, 15, 16, 24, 18, 19, 14, 20, 17, 21, 23, 22, 848, 854, 853, 857, 855, 852, 847, 856, 851, 849, 858, 850, 26, 27, 35, 31, 37, 32, 36, 38, 39, 30, 507, 509, 514, 508, 510, 512, 506, 516, 521, 523, 525, 527, 529, 538, 531, 536, 423, 421, 427, 429, 435, 437, 441, 445, 443,   255, 252, 259, 279, 280, 276, 261, 283,      265, 267, 269, 271, 272, 274, 286,       307, 309, 293, 297, 299, 300, 313,      321, 325, 327, 330, 332, 318, 315, 317, 304,    303, 290, 464, 467,       489, 487, 459, 476, 492, 480, 498, 473, 484, 496, 502, 546, 548, 549, 550, 561, 560, 552, 553, 555, 566, 564, 567, 558, 556, 569, 568, 574, 573, 584, 583, 575, 577, 578,      586, 588, 579, 652, 659, 660, 658, 665, 664, 663, 667, 668, 669, 679, 678, 677, 674, 672, 673, 683, 682, 681, 695, 694, 693, 687, 686, 689, 690, 691, 685, 700, 701, 699,    883, 885, 884, 877, 875, 876, 881, 880, 879, 870, 871, 869, 874, 873, 872, 887, 888, 889, 890, 891, 892, 896, 897, 898, 901, 900, 899, 895, 894, 893, 45, 44, 43, 42, 41, 40, 53, 54, 52, 56, 57, 55, 47, 48, 46, 51, 50, 49, 68, 69, 70, 71, 72, 74, 63, 64, 65, 61, 60, 59, 80, 81, 82, 85, 89, 86, 75, 76, 77, 158, 160, 161, 153, 154, 155, 163, 164, 165, 183, 185, 186, 189, 190, 193, 197, 198, 200, 194, 168, 169, 170, 174, 178, 179, 732, 737, 734, 735, 724, 723, 752, 751, 748, 750, 749, 745, 743, 746, 835, 842, 841, 843, 845, 846, 837, 145, 146, 150, 102, 103, 96, 94, 101, 100, 90, 803, 809, 804, 808, 91, 92, ]
+annotations_list = []
 images_list = []
 category_dict = { }
 
@@ -74,8 +75,10 @@ def combine():
                         combine_json['annotations'].append(annotations)
 
                 images['id'] = f"temp_{i:>06}"
-
-                os.rename(images['file_name'], f'temp_{i:>06}.jpg')
+                try:
+                    os.rename(images['file_name'], f'temp_{i:>06}.jpg')
+                except:
+                    pass
                 rename_list.write(images['file_name'] + f' / {i:>06}.jpg \n')
                 images['file_name'] = f'temp_{i:>06}.jpg'
                 combine_json['images'].append(images)
@@ -116,15 +119,19 @@ def filter(arg):
     for annotations in all_json['annotations']:
         is_pass = True
 
-        # check annotations['id'] is in the annotations_list
-        for annotation_id in annotations_list:
-            if annotations['id'] == f"temp_{annotation_id:>06}":
-                is_pass = False
-                annotations_list.remove(annotation_id)
+        # comment for filter image don't have annotation
 
-        # if annotations['id'] is not in the annotations_list
-        if is_pass: 
-            j += 1
+        # # check annotations['id'] is in the annotations_list
+        # for annotation_id in annotations_list:
+        #     if annotations['id'] == f"temp_{annotation_id:>06}":
+        #         is_pass = False
+        #         annotations_list.remove(annotation_id)
+
+        # # if annotations['id'] is not in the annotations_list
+        # if is_pass: 
+        #     j += 1
+
+        if annotations['category_id'] == 1:
             area_filter_json['annotations'].append(annotations)
 
             images_list.append(annotations["image_id"])
@@ -138,6 +145,7 @@ def filter(arg):
                 is_pass = True
                 images_list.remove(image_id)
                 area_filter_json['images'].append(images)
+                break
 
         # if images['id'] is not in the images_list
         if not is_pass:
